65f27d1b47dee2d4134a7b0b87ba22be9b1c9edf
Sz4by <szaby.korodi@yahoo.com>
2024-10-27 12:51:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
- const newswire = require('./newswire');
- const latestNews = new newswire("gtav", "https://...");
+const newswire = require('./newswire');
+const latestNews = new newswire("gtav", "https://...");
+const keep_alive = require('./keep_alive.js')
