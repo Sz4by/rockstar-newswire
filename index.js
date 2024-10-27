d939fcb35b0f8625f3b7b9d9ae5bf748c367ebe3
Sz4by <szaby.korodi@yahoo.com>
2024-10-27 15:19:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 const newswire = require('./newswire');
 const latestNews = new newswire("gtav", "https://discord.com/api/webhooks/1300049344442929182/Suh0XEsbVa4WDMNkgpQM8CBDF1qmk8ruE4klziJwHJelmsGU4QH4nRapor2sIFWqCJSp");
-const keep_alive = require('./keep_alive.js')
