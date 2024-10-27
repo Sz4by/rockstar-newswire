84754181970efdd58d1985390ec1e11d7c6581d6
Sz4by <szaby.korodi@yahoo.com>
2024-10-27 15:49:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const newswire = require('./newswire');
-const latestNews = new newswire("gtav", "https://discord.com/api/webhooks/1300049344442929182/Suh0XEsbVa4WDMNkgpQM8CBDF1qmk8ruE4klziJwHJelmsGU4QH4nRapor2sIFWqCJSp");
+const latestNews = new newswire("gtav", "https://discord.com/api/webhooks/1300093837233885204/roto6rlFo3x6W3iF4lzMyVuE4Hv4f5C28lALtOOMqGNwPAlG6bjHNOp9YfMzqusamUwR");
