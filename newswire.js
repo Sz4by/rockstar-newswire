a62223f26adae8ec189c1b1007af776c7b1fa720
Sz4by <szaby.korodi@yahoo.com>
2024-10-27 13:04:22 +0200

    Update newswire.js

--- newswire.js
+++ newswire.js
@@ -1,3 +1,6 @@
+const newswire = require('./newswire');
+const latestNews = new newswire("gtav", "https://discord.com/api/webhooks/1300049344442929182/Suh0XEsbVa4WDMNkgpQM8CBDF1qmk8ruE4klziJwHJelmsGU4QH4nRapor2sIFWqCJSp");
+const keep_alive = require('./keep_alive.js')
 const genres = {
     latest: null,
     music: 30,
